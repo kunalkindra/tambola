7aa5536328b8180e18d265ce768784885ca9d4ae
Kunal Kindra <kunal.kindra@zalando.de>
2020-04-12 12:21:28 +0200

    Pulse animation for last number

--- src/components/NumbersTable/NumbersTable.js
+++ src/components/NumbersTable/NumbersTable.js
@@ -1,13 +1,21 @@
+import { last } from '../../utils/last';
+
 export default function NumbersTable({ numbers }) {
 	const boxes = [];
 	for (let i = 1; i <= 90; i++) {
-		let activeClass = numbers.includes(i) ? 'numbers-table__box--active' : '';
-		boxes.push(<div className={`numbers-table__box ${activeClass}`}><span>{i}</span></div>)
+		let classNames = [`numbers-table__box`];
+		if (numbers.includes(i)) {
+			classNames.push('numbers-table__box--active');
+		}
+		if (last(numbers) === i) {
+			classNames.push('numbers-table__box--last');
+		}
+		boxes.push(<div className={classNames.join(' ')}><span>{i}</span></div>);
 	}
 	
 	return (
 		<div className="numbers-table">
 			{boxes}
 		</div>
-	)
+	);
 }
