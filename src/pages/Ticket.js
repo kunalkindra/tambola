9a0fcbe8c63b5172bcefa71a1284bd422cf725b9
Kunal Kindra <kunal.kindra@zalando.de>
2020-04-18 14:22:21 +0200

    Add name, remaining numbrs and update ticket styles

--- src/pages/Ticket.js
+++ src/pages/Ticket.js
@@ -1,5 +1,6 @@
 import { Component } from 'preact';
 import { tickets } from '../constants/tickets';
+import { zeroPad } from '../utils/zeroPad';
 
 
 export default class Ticket extends Component {
@@ -14,25 +15,34 @@ export default class Ticket extends Component {
 	};
 	render() {
 		const id = this.props.id;
-		const numbers = tickets[id];
+		const { numbers, lastName, salutation } = tickets[id];
+		const checkedNumbers = this.state.checkedNumbers;
+		let remainingNumbers = 15 - checkedNumbers.length;
 		return (
-			<table className="ticket table table-bordered text-center">
-				{numbers.map(line => (
-					<tr>
-						{line.map(number => {
-							const classes = ['ticket__number', 'px-0'];
-							let checked = this.state.checkedNumbers.includes(number);
-							if (checked)
-								classes.push('ticket__number--checked');
-							return (
-								<td onClick={this.toggleCheck} data-number={number} className={classes.join(' ')}>
-									{number}
-								</td>
-							);
-						})}
-					</tr>
-				))}
-			</table>
+			<div>
+				<div className="d-flex justify-content-between align-items-baseline">
+					<h4 className="mb-0">Hello {salutation} {lastName}!</h4>
+					<div>Remaining - <span className="bg-info text-white p-1 rounded h6">{zeroPad(remainingNumbers)}</span></div>
+				</div>
+				<hr />
+				<table className="ticket table table-bordered text-center">
+					{numbers.map(line => (
+						<tr>
+							{line.map(number => {
+								const classes = ['ticket__number', 'px-0'];
+								let checked = checkedNumbers.includes(number);
+								if (checked)
+									classes.push('ticket__number--checked');
+								return (
+									<td onClick={this.toggleCheck} data-number={number} className={classes.join(' ')}>
+										{number}
+									</td>
+								);
+							})}
+						</tr>
+					))}
+				</table>
+			</div>
 		);
 	}
 }
---
+++ src/utils/zeroPad.js
@@ -0,0 +1,3 @@
+export function zeroPad(number) {
+	return number < 10 ? `0${number}` : number;
+}
